c490af72513be076de33868e200e58860ec1109d
Hoàng Kiên <126592782+Hkzeuss@users.noreply.github.com>
2024-12-25 17:52:48 +0700

    add router Product Detail

--- vju-sport/src/App.js
+++ vju-sport/src/App.js
@@ -5,13 +5,14 @@ import LandingPage from './pages/LandingPage/LandingPage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import RegisterPage from './pages/RegisterPage/RegisterPage';
 import ProductListPage from './pages/ProductListPage/ProductListPage';
+import ProductDetail from './pages/ProductDetail/ProductDetail';
 
 
 import './pages/LandingPage/LandingPage.css';
 import './pages/LoginPage/LoginPage.css';
 import './pages/RegisterPage/RegisterPage.css';
 import './pages/ProductListPage/ProductListPage.css';
-
+import './pages/ProductDetail/ProductDetail.css';
 const App = () => {
   return (
     <Router>
@@ -20,6 +21,7 @@ const App = () => {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/productslist" element={<ProductListPage />} />
+        <Route path="/productdetail" element={<ProductDetail />} />
       </Routes>
     </Router>
   );
